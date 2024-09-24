d702f2c384807502bdfbbf8b0f9f1167b248645e
Jan R <Jackjan@protonmail.com>
2024-09-24 08:22:02 +0200

    Added Service interface

---
+++ MvvmUtils/IInitializableService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Roslan.DotNetUtils.Mvvm {
+    public interface IInitializableService {
+
+
+
+        /// <summary>
+        /// Determines whether the service has been initialized.
+        /// </summary>
+        bool IsInitialized { get; }
+
+
+
+        void Initialize();
+
+
+
+        void Deinitialize();
+    }
+}
---
+++ MvvmUtils/IInitializableServiceAsync.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Roslan.DotNetUtils.Mvvm {
+    public interface IInitializableServiceAsync {
+
+
+
+        /// <summary>
+        /// Determines whether the service has been initialized.
+        /// </summary>
+        bool IsInitialized { get; set; }
+
+
+
+        Task InitializeAsync();
+
+
+
+        Task DeInitializeAsync();
+    }
+}
