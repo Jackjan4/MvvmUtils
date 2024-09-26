0c6025fc75e5172ba4a246ae76d62bf71c381f74
Jan R <Jackjan@protonmail.com>
2024-09-26 09:08:27 +0200

    Moved Service interfaces to correct namespace

--- MvvmUtils/IInitializableServiceAsync.cs
+++ MvvmUtils/IInitializableServiceAsync.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 
-
-
-namespace Roslan.DotNetUtils.Mvvm {
+namespace Roslan.MvvmUtils {
     public interface IInitializableServiceAsync {
 
 
