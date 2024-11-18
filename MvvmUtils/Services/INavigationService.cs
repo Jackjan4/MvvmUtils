b6a77f9ef7fbcd66895516718789ea48f5c2915f
J R <Jackjan@protonmail.com>
2024-11-18 10:21:09 +0100

    Added BlazorNavigationService

---
+++ MvvmUtils/Services/INavigationService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Roslan.MvvmUtils.Services {
+    public interface INavigationService {
+
+        void NavigateTo(string pagename);
+
+    }
+}
---
+++ Mvvmutils.Blazor/Services/BlazorNavigationService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Roslan.MvvmUtils.Services;
+
+
+
+namespace Mvvmutils.Blazor.Services;
+
+
+
+public class BlazorNavigationService : INavigationService {
+
+
+
+    #region "Services"
+    private readonly NavigationManager _navigationManager;
+    #endregion
+
+
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="navigationManager"></param>
+    public BlazorNavigationService(NavigationManager navigationManager) {
+        _navigationManager = navigationManager;
+    }
+
+
+
+    public void NavigateTo(string pageName) {
+        _navigationManager.NavigateTo(pageName);
+    }
+
+}
